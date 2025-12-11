768f443edb21f306cfccb94cfb303c5c1d6501ab
Your Name <bblazev@algebra.hr>
2025-12-11 11:09:37 +0100

    Initial commit, login/register page

---
+++ MentorEval/Models/Answer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MentorEval.Models;
+
+public partial class Answer
+{
+    public int Id { get; set; }
+
+    public int? Grade { get; set; }
+
+    public string? Text { get; set; }
+
+    public DateTime SubmittedAt { get; set; }
+
+    public int QuestionId { get; set; }
+
+    public int EvaluationId { get; set; }
+
+    public int StudentId { get; set; }
+
+    public virtual Evaluation Evaluation { get; set; } = null!;
+
+    public virtual Question Question { get; set; } = null!;
+
+    public virtual User Student { get; set; } = null!;
+}
